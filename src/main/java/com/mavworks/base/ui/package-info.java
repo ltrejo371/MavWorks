38f002fc1680c82f584076b765611827b1c7ab8c
start.vaadin.com <no-reply@vaadin.com>
2026-09-18 23:27:42 +0000

    Generated project

---
+++ src/main/java/com/mavworks/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.mavworks.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/com/mavworks/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package com.mavworks.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
