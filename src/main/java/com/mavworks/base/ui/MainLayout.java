c9389d9e5c2e858adacc3f37fd286da0846725fe
ltrejo371 <144194487+ltrejo371@users.noreply.github.com>
2026-09-18 19:02:56 -0500

    Initial set up -  MavWorks Vaadin + Springboot

--- src/main/java/com/mavworks/base/ui/MainLayout.java
+++ src/main/java/com/mavworks/base/ui/MainLayout.java
@@ -25,11 +25,11 @@ public final class MainLayout extends AppLayout {
 
     private Component createApplicationHeader() {
         // TODO Replace with real application logo and name
-        var appLogo = new Avatar("My Application");
+        var appLogo = new Avatar("MavWorks");
         appLogo.addClassName("app-logo");
         appLogo.addThemeVariants(AvatarVariant.AURA_FILLED, AvatarVariant.XSMALL);
 
-        var appName = new Span("My Application");
+        var appName = new Span("MavWorks");
         appName.addClassName("app-name");
 
         var header = new HorizontalLayout(appLogo, appName);
